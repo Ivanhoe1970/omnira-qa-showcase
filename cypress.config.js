778e4c04eee29785d569fa7c34018110d25e3406
Ivan Ferrer <ivanferrer@Ivans-MacBook-Pro.local>
2025-03-31 22:22:21 -0600

    CI/CD: wait for app and API before Cypress tests

--- cypress.config.js
+++ cypress.config.js
@@ -2,7 +2,8 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: "http://localhost:3004",  
+    baseUrl: "http://localhost:3000",  
     supportFile: false,
-  }
+  },
+  ci: true
 });
